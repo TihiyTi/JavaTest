4ff19a5d27abd78bf4aabd6275a5d3e4b741198a
TihiyTi <tihiy-ti@mail.ru>
2014-09-26 18:03:31 +0400

    FXTest + Spring games

--- FXTest/src/main/java/com/tihiy/fxtest/Controller.java
+++ FXTest/src/main/java/com/tihiy/fxtest/Controller.java
@@ -1,6 +1,8 @@
 package com.tihiy.fxtest;
 
 import javafx.beans.binding.Bindings;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
@@ -12,15 +14,26 @@ import java.util.ResourceBundle;
 
 public class Controller implements Initializable{
 
+    StringProperty bufProp1 = new SimpleStringProperty();
+//    StringProperty bufProp2 = new SimpleStringProperty();
+
     public Label label;
+    public Label label2;
     public TextField textField;
     public FlowPane pane;
     public Button button;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-        Bindings.bindBidirectional(label.textProperty(), textField.textProperty());
+        System.out.println("Initialize label "+ label.toString());
+        Bindings.bindBidirectional(label.textProperty(), bufProp1);
+        Bindings.bindBidirectional(label2.textProperty(), bufProp1);
+        Bindings.bindBidirectional(textField.textProperty(), bufProp1);
     }
 
+    public void setBindProperty(StringProperty property){
+        Bindings.bindBidirectional(bufProp1, property);
+//        Bindings.bindBidirectional(bufProp2, property);
+    }
 
 }
--- FXTest/src/main/java/com/tihiy/fxtest/FXTest.java
+++ FXTest/src/main/java/com/tihiy/fxtest/FXTest.java
@@ -1,10 +1,18 @@
 package com.tihiy.fxtest;
 
+import com.sun.javafx.css.CssError;
 import javafx.application.Application;
+import javafx.beans.binding.Bindings;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
+import org.springframework.beans.factory.ListableBeanFactory;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -12,6 +20,12 @@ import java.util.ResourceBundle;
 public class FXTest extends Application {
     private static final String SCENE_XML = "scene.fxml";
     private static final String KEYS_PROPS = "keys";
+    private static final String FX_CONTEXT_XML = "context.xml";
+
+
+    private final ListableBeanFactory context = new ClassPathXmlApplicationContext(
+            getClass().getPackage().getName().replaceAll("\\.", "/") + "/" + FX_CONTEXT_XML
+    );
 
     public static void main(String[] args) {
         launch();
@@ -20,7 +34,6 @@ public class FXTest extends Application {
     @Override
     public void start(Stage stage) throws Exception {
         try{
-
             Class clazz = FXTest.class;
             URL location1 = clazz.getResource(SCENE_XML);
             FXMLLoader fxmlLoader = new FXMLLoader(location1);
@@ -28,6 +41,11 @@ public class FXTest extends Application {
 
             stage.setScene(new Scene(root));
             stage.show();
+
+            Controller mc = (Controller) context.getBean("mc");
+            StringProperty prop = new SimpleStringProperty();
+            mc.setBindProperty(prop);
+            prop.addListener((observable, oldValue, newValue) -> System.out.println("TEST COMPLETE!!"));
         }catch (Exception e){
             System.out.println(e.toString());
             e.printStackTrace();
